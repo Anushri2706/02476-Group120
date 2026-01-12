d5a0663c80aeb11f8640cf8d0abc5ef2a930e502
Toma <thomas.guci@gmail.com>
2026-01-12 22:33:20 +0100

    meh

--- src/mlops/train.py
+++ src/mlops/train.py
@@ -1,9 +1,9 @@
-from mlops.model import Model
-from mlops.data import MyDataset
+from src.mlops.model import Model
+from src.mlops.data import MyDataset
 
 def train():
     dataset = MyDataset("data/processed")
-    model = Model()
+    model = Model(num_classes= 43)
     # add rest of your training code here
 
 if __name__ == "__main__":
