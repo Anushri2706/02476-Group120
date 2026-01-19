f45d6dad427c2d907cc3356b7c0ce8039d814fca
Toma <91958716+guci-toma@users.noreply.github.com>
2026-01-19 13:18:50 +0100

    Toma (#17)
    * added logging to train.py
    
    * flag
    *working training logic

--- src/mlops/train.py
+++ src/mlops/train.py
@@ -5,11 +5,14 @@ from torchvision import transforms
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import logging
 
 # Assuming your GTSRB class is in a file named `dataset.py`
 from .data.dataset import GTSRB
 from .model import TinyCNN
 
+# Initialize logger
+log = logging.getLogger(__name__)
 
 @hydra.main(config_path="../../configshydra", config_name="config")
 def main(cfg: DictConfig):
@@ -40,20 +43,22 @@ def main(cfg: DictConfig):
     )
 
     # 3. Create DataLoaders
-    train_loader = DataLoader(train_ds, batch_size=cfg.training.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    train_loader = DataLoader(
+        train_ds, batch_size=cfg.training.batch_size, shuffle=True, num_workers=4, pin_memory=True
+    )
 
     val_loader = DataLoader(val_ds, batch_size=cfg.training.batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
     test_loader = DataLoader(test_ds, batch_size=cfg.training.batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
     # --- Verification (Optional) ---
-    print(f"Train Dataset size: {len(train_ds)}")
-    print(f"Validation Dataset size: {len(val_ds)}")
+    log.info(f"Train Dataset size: {len(train_ds)}")
+    log.info(f"Validation Dataset size: {len(val_ds)}")
 
     # Try fetching one batch to ensure paths and transforms work
     images, labels = next(iter(train_loader))
-    print(f"Batch Image Shape: {images.shape}")  # Should be [32, 3, 32, 32]
-    print(f"Batch Label Shape: {labels.shape}")
+    log.info(f"Batch Image Shape: {images.shape}")  # Should be [32, 3, 32, 32]
+    log.info(f"Batch Label Shape: {labels.shape}")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -73,33 +78,7 @@ def main(cfg: DictConfig):
             loss.backward()
             optimizer.step()
 
-        print(f"Epoch {epoch+1}: loss = {loss.item():.4f}")
-
-
-
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    model = TinyCNN(num_classes= 43)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
-
-
-
-    epochs = 1
-    for epoch in range(epochs):
-        model.train()
-
-        for img, labels in train_loader:
-            img, labels = img.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(img)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-        print(f"Epoch {epoch+1}: loss = {loss.item():.4f}")
-
-
+        log.info(f"Epoch {epoch+1}: loss = {loss.item():.4f}")
 
 
 if __name__ == "__main__":
